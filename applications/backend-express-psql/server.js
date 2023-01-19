998e89a73d2e87a3da8879cef6df2a1f5cebd8e8
kandreasyan <65715155+kandreasyan@users.noreply.github.com>
2023-01-19 22:13:03 +0400

    hardcode cors origin

--- applications/backend-express-psql/server.js
+++ applications/backend-express-psql/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+  origin: process.env.CLIENT_ORIGIN || "*"
 };
 
 app.use(cors(corsOptions));
